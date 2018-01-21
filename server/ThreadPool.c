463082f538e1192103c4c781cbf2654ad682b050
bagnosh <shohambagno@gmail.com>
2018-01-21 16:23:52 +0200

    switch again!

--- server/ThreadPool.c
+++ server/ThreadPool.c
@@ -16,4 +16,35 @@ ThreadPool::ThreadPool(int threadsNum) : stopped(false) {
 	pthread_mutex_init(&lock, NULL);
 }
 
+void* ThreadPool::execute(void *arg) {
+	ThreadPool *pool = (ThreadPool *)arg;
+	pool->executeTasks();
+}
 
+void ThreadPool::addTask(Task *task) {
+	taskQueue.push(task);
+}
+
+void ThreadPool::executeTasks() {
+	whili (!stopped) {
+		pthread_mutex_lock(&lock);
+		if (!taskQueue.empty()) {
+			Task* task = taskQueue.front();
+			taskQueue.pop();
+			pthread_mutex_unlovk(&lock);
+			task->execute();
+		} else {
+			pthread_mutex_unlock(&lock);
+			sleep(1);
+		}
+	}
+}
+
+void ThreadPool::terminate() {
+	pthread_mutex_destroy(&lock);
+	stopped = true;
+}
+
+ThreadPool::~ThreadPool() {
+	delete[] threads;
+}
